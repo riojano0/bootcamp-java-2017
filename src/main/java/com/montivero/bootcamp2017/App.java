9348e72fc3f73504abb5a429bd9db0ee9d8283b5
riojano0 <danielelnuevo@hotmail.com>
2017-01-14 00:46:42 -0300

    Fix dirs

---
+++ src/main/java/com/montivero/bootcamp2017/App.java
@@ -0,0 +1,13 @@
+package com.montivero.bootcamp2017;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
